92cbaabee2881b9d14fbc0d7e76383540bb1379a
daisyalexandria <dazavala7@gmail.com>
2022-08-17 21:14:21 -0500

    created all basic routes, added new html and show html files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   root "articles#index"
-  get "/articles" => "articles#index"
 
+  resources :articles
 
 end
